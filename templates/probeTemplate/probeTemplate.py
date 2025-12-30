200aa2cb839b535fa84c626e7675073c6f62fc3d
Jovan Kemp <jtk194@gmail.com>
2025-12-29 20:10:35 -0500

    Creation of 2D probe procedure for measuring depth
    * Creation of 2D probe generator
    
    * Probe adjustment template

--- templates/probeTemplate/probeTemplate.py
+++ templates/probeTemplate/probeTemplate.py
@@ -2,13 +2,15 @@
 from psychopy import core, gui
 from psychopy.data import ExperimentHandler, TrialHandler
 from psychopy.hardware import keyboard
+import numpy as np
 
 # Class built functions
 import threedipa.utils as utils
 from threedipa.renderer.haploscopeRender import HaplscopeRender2D
 from threedipa.renderer.haploscopeConfig import monitor_settings, physical_calibration
-from threedipa.procedure import OneIntervalDraw
+from threedipa.procedure import stimulusAdjustmentDraw
 from threedipa.stimuli.stimulus2D import Stimulus2DImage
+from threedipa.stimuli.probe2D import ShapeOutlineProbe
 
 # Exit protocols
 def exit_experiment(phaseTracker):
@@ -25,13 +27,11 @@ def trialPhase_timing(phaseTracker: utils.PhaseTracker, trial_time, config):
         # For the first part of presentation, show the fixation cross
         if trial_time.getTime() <= config['FixationDuration']:
             phaseTracker.set_stimulus_phase(utils.StimulusPhase.FIXATION)
-        # For the second part of presentation, show the stimulus
-        elif trial_time.getTime() <= config['StimulusDuration'] + config['FixationDuration']:
+        # For the second part of presentation, show the stimulus and probe
+        elif trial_time.getTime() > config['FixationDuration']:
             phaseTracker.set_stimulus_phase(utils.StimulusPhase.STIMULUS)
-        # Do not draw any stimulus and wait for a response
-        elif trial_time.getTime() > config['StimulusDuration'] + config['FixationDuration']:
-            phaseTracker.set_stimulus_phase(utils.StimulusPhase.NONE)
             phaseTracker.set_response_phase(utils.ResponsePhase.WAIT_FOR_RESPONSE)
+            
 
 def load_stimulus(
     stimulusDepth: float, 
@@ -56,7 +56,7 @@ def main():
     dlg = gui.DlgFromDict(info, title='Johnston Stereopsis')
 
     # 2. Load parameters
-    exp_dir = './templates/johnstonTemplate/'
+    exp_dir = './templates/probeTemplate/'
     data_file_name = exp_dir + f"data/johnston_{info['Participant ID']}_{info['Session']}"
     parameters = utils.parse_parameters_file(exp_dir + 'parameters.txt')
     debug_mode = parameters['parameters']['Debug']
@@ -66,6 +66,11 @@ def main():
     repetitions = parameters['parameters']['Repetitions']
     fixationDistance = parameters['parameters']['FixationDistance']
     stimulusVisualAngle = parameters['parameters']['VisualAngle']
+
+    # Probe parameters
+    probeLengthDegree = parameters['parameters']['ProbeLength']
+    magnitude_step = parameters['parameters']['ProbeLength']
+    probe_y_position_degrees = parameters['parameters']['ProbeYPosition']
     
     # 4. Create ExperimentHandler and TrialHandler
     thisExp = ExperimentHandler(
@@ -98,25 +103,50 @@ def main():
         screen_config=monitor_settings,
         debug_mode=debug_mode,
     )
+
+    # 7. Define probe function and probe
+    # (Function is defined here to take advantage of the renderer conversion function)
+    probe_length = renderer.convert_visual_angle_to_pixels(probeLengthDegree)
+    probe_half_length = probe_length / 2
+    def parabolaProbe(x: float, magnitude: float) -> float:
+        """
+        Function that controls the stretch/compression of parabola probe.
+        """
+        y = (2*np.sqrt(x**2) / probe_half_length)**2 * magnitude
+        return y
+    
+    # Create probe with parabolaProbe function
+    magnitude = 0 # In centimeters
+    probe_y_position_pixels = renderer.convert_visual_angle_to_pixels(probe_y_position_degrees)
+    probe = ShapeOutlineProbe(
+        probe_func=parabolaProbe,
+        magnitude=magnitude,
+        x_range=(-probe_half_length, probe_half_length),
+        segments=100,
+        position=[0, -probe_y_position_pixels],
+        line_width_pix=3,
+        rotate_90=True,
+        color='white'
+    )
+    
     kb = keyboard.Keyboard(clock=core.Clock())
     # initialize recording variables
     trial_time = core.Clock()
     rt = 0.0
-    response_key = None
 
-    # 7. Calibrate the physical haploscope
+    # 8. Calibrate the physical haploscope
     renderer.draw_physical_calibration()
     renderer.render_screen()
     kb.waitKeys(keyList=['return', ], waitRelease=True)
     
-    # 8. Give instructions to the participant
+    # 9. Give instructions to the participant
     instructions_text = ("Press 3 if the stimulus is stretched and 6 if it is squashed."
     + "\nPress enter to begin the experiment.")
     renderer.draw_text(instructions_text, pos=(0, 0))
     renderer.render_screen()
     kb.waitKeys(keyList=['return'], waitRelease=True)
     
-    # 9. Experiment loop to run each trial
+    # 10. Experiment loop to run each trial
     # Iterate through the TrialHandler
     for currentTrial in trials:
         # Break trial loop if the experiment should be ended
@@ -142,35 +172,33 @@ def main():
             trialPhase_timing(phaseTracker, trial_time, parameters['parameters'])
             
             # Run the draw function
-            OneIntervalDraw(renderer, stimulus, phaseTracker.get_stimulus_phase())
+            stimulusAdjustmentDraw(renderer, stimulus, probe, phaseTracker.get_stimulus_phase())
 
             if phaseTracker.get_response_phase() == utils.ResponsePhase.WAIT_FOR_RESPONSE:
-                kb.clock.reset()
-                kb.clearEvents()
-                response_key = kb.waitKeys(keyList=['num_3', '3', '6', 'num_6', 'escape'], waitRelease=False)
-                
-                if response_key:
-                    rt = kb.clock.getTime()
-                    phaseTracker.set_response_phase(utils.ResponsePhase.RESPONSE_RECEIVED)
-                    break
-
-        if response_key[0].name == 'escape':
-            # Exit the experiment
-            exit_experiment(phaseTracker)
-            break
-        else:
-            phaseTracker.set_experiment_phase(utils.ExperimentPhase.POST_TRIAL)
+                response_keys = kb.getKeys(['a', 's', 'escape', 'return'], waitRelease=False)
         
-
-        # Remove 'num_' prefix from key name if input is from numpad
-        response_name = response_key[0].name
-        if response_name.startswith('num_'):
-            response_name = response_name[4:]  # Remove first 4 characters ('num_')
-
+                if response_keys:
+                    key = response_keys.pop()
+                    if key.name == 'escape':
+                        exit_experiment(phaseTracker)
+                        break
+                    elif key.name == 'a':
+                        magnitude = max(0, magnitude - magnitude_step)
+                        probe.setMagnitude(magnitude)
+                    elif key.name == 's':
+                        magnitude += magnitude_step
+                        probe.setMagnitude(magnitude)
+                    elif key == 'return':
+                        rt = kb.clock.getTime()
+                        phaseTracker.set_stimulus_phase(utils.StimulusPhase.NONE)
+                        phaseTracker.set_response_phase(utils.ResponsePhase.RESPONSE_RECEIVED)
+                        phaseTracker.set_experiment_phase(utils.ExperimentPhase.POST_TRIAL)
+                        break
+        
 
         # Store data and end the entry line
         trials.addData('stimulus_id', getattr(stimulus, 'stimulus_id', ''))
-        trials.addData('response_key', response_name)
+        trials.addData('magnitude', magnitude)
         trials.addData('rt_s', rt)
         
         # Move to the next line of the experimentHandler data management
